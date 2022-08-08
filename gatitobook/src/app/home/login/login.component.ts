039c7818711b73fa7e680cadfb0094fda945a096
Breno <brenogpa@gmail.com>
2022-08-08 16:38:44 -0300

    consuming authentication service

--- gatitobook/src/app/home/login/login.component.ts
+++ gatitobook/src/app/home/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
@@ -9,12 +10,19 @@ export class LoginComponent implements OnInit {
   usuario = '';
   senha = '';
 
-  constructor() {}
+  constructor(private authService: AutenticacaoService) {}
 
   ngOnInit(): void {}
 
   login() {
-    console.log(this.usuario);
-    console.log(this.senha);
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      () => {
+        console.log('Autenticado com sucesso');
+      },
+      (error) => {
+        alert('Usuário ou senha Inválido');
+        console.log(error);
+      }
+    );
   }
 }
