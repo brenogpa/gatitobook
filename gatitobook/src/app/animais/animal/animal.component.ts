b315b6e876cb890f6091edbaa7dcf8ba1edf1359
Breno <brenogpa@gmail.com>
2022-08-29 11:47:57 -0300

    componente/module cartao

--- gatitobook/src/app/animais/animal/animal.component.ts
+++ gatitobook/src/app/animais/animal/animal.component.ts
@@ -5,16 +5,15 @@ const API = 'http://localhost:3000';
 @Component({
   selector: 'app-animal',
   templateUrl: './animal.component.html',
-  styleUrls: ['./animal.component.css']
+  styleUrls: ['./animal.component.css'],
 })
 export class AnimalComponent implements OnInit {
-
   private urlOriginal = '';
 
   @Input() descricao = '';
 
   @Input() set url(url: string) {
-    if(url.startsWith('data')) {
+    if (url.startsWith('data')) {
       this.urlOriginal = url;
     } else {
       this.urlOriginal = `${API}/imgs/${url}`;
@@ -25,9 +24,7 @@ export class AnimalComponent implements OnInit {
     return this.urlOriginal;
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
