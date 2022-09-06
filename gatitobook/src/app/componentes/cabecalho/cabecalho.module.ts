866ca8cb5856c718c969a40583f3b6523a1d3c2f
Breno <brenogpa@gmail.com>
2022-09-06 15:22:33 -0300

    adicionado menu ao cabecalho

--- gatitobook/src/app/componentes/cabecalho/cabecalho.module.ts
+++ gatitobook/src/app/componentes/cabecalho/cabecalho.module.ts
@@ -2,19 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CabecalhoComponent } from './cabecalho.component';
 import { RouterModule } from '@angular/router';
-
-
+import { MenuModule } from '../menu/menu.module';
 
 @NgModule({
-  declarations: [
-    CabecalhoComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
-  exports: [
-    CabecalhoComponent,
-  ]
+  declarations: [CabecalhoComponent],
+  imports: [CommonModule, RouterModule, MenuModule],
+  exports: [CabecalhoComponent],
 })
-export class CabecalhoModule { }
+export class CabecalhoModule {}
